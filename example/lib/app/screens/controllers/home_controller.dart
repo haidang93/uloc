93917459ebc76c181d0d0155073ea5a787aab8d7
Nguyen Hai Dang <dannynguyen.vnn@gmail.com>
2025-07-05 17:50:35 +0700

    update

--- example/lib/app/screens/controllers/home_controller.dart
+++ example/lib/app/screens/controllers/home_controller.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_overrides
+
 import 'package:uloc/uloc.dart';
 
 class HomeController extends ULoCProvider {
@@ -19,5 +21,4 @@ class HomeController extends ULoCProvider {
   void onDispose() {
     super.onDispose();
   }
-
 }
